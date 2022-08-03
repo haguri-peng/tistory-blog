b597082ed055d71fabc8972d03a4c6b35faf7593
haguri <hty851111@naver.com>
2022-08-03 14:30:20 +0900

    색상 반대로 변경

--- components/LoadingSpinner.vue
+++ components/LoadingSpinner.vue
@@ -19,8 +19,8 @@ export default {};
   width: 50px;
   height: 50px;
   border-radius: 50%;
-  border: 5px solid #f9ebc8;
-  border-bottom: 5px solid #a0bcc2;
+  border: 5px solid #a0bcc2;
+  border-bottom: 5px solid #f9ebc8;
   animation: spin 1s linear infinite;
   position: relative;
 }
