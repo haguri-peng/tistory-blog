a2b326b55e71dad1f15d3ed2308f1fbcf838aac5
haguri <hty851111@naver.com>
2022-08-05 16:22:56 +0900

    add AppPost, SearchInput

---
+++ components/AppPost.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>
+    <div class="post-id">ID: {{ postItem.id }}</div>
+    <div class="post-title">
+      <span>{{ postItem.title }}</span>
+      <span>[{{ postItem.comments }}]</span>
+    </div>
+    <div class="post-url">URL: {{ postItem.postUrl }}</div>
+    <div class="post-date">작성일자: {{ postItem.date }}</div>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    postItem: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+li {
+  display: inline-block;
+  width: 30%;
+  border: 3px solid #73a9ad;
+  margin: 10px 10px;
+}
+</style>
---
+++ components/SearchInput.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="input-wrapper">
+    <input
+      type="text"
+      class="search-input"
+      v-model="keyword"
+      @keyup.enter="$emit('search', $event.target.value)"
+    />
+    <button type="button" class="btn" @click="searchKeyword">Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      keyword: '',
+    };
+  },
+  methods: {
+    searchKeyword() {
+      this.$emit('search', this.keyword);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.input-wrapper {
+  height: 40px;
+  margin: 1rem 0;
+}
+.search-input {
+  width: 200px;
+  font-size: 1.2rem;
+  font-weight: 500;
+}
+.btn {
+  pointer-events: all;
+  font-size: 1.2rem;
+  font-weight: 500;
+}
+</style>
