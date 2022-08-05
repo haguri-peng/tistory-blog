41c4a35972b4dd34f5a543545fbe68a09ea48152
haguri <hty851111@naver.com>
2022-08-05 16:24:21 +0900

    검색된 글 목록을 가져옴 (최대 10개). 세팅까진 하지 않음

--- components/AppCategory.vue
+++ components/AppCategory.vue
@@ -1,37 +1,88 @@
 <template>
-  <div>Category Page</div>
+  <search-input @search="searchPosts"></search-input>
+  <div class="posts">
+    <ul>
+      <app-post
+        v-for="post in postList"
+        :key="post.id"
+        :postItem="post"
+      ></app-post>
+    </ul>
+  </div>
 </template>
 
 <script>
-import { fetchPostListByCategory } from '../api/index';
+import SearchInput from './SearchInput.vue';
+import AppPost from './AppPost.vue';
+import { fetchPostListByCategory, fetchPostList } from '../api/index';
 import _ from 'lodash';
 
 export default {
+  components: {
+    SearchInput,
+    AppPost,
+  },
   data() {
     return {
       postList: [],
+      searchPostList: [],
     };
   },
   methods: {
-    async fetchPostByCategory() {
+    async fetchPostByCategory(pageNum) {
       const { data } = await fetchPostListByCategory(
         this.$route.params.categoryId,
-        1
+        pageNum || 1
       );
       // console.log(data);
 
       // 발행된 건만
-      this.postList = _.filter(
-        data.tistory.item.posts,
-        (p) => p.visibility == '20'
-      );
+      this.postList = _.filter(data.tistory.item.posts, ['visibility', '20']);
+    },
+    async searchPosts(keyword, pageNum) {
+      const tmpPosts = [];
+      const limitCnt = 10;
+      const maxPageNum = Math.ceil($vm.data.postCnt / 10);
+
+      for (
+        pageNum = pageNum || 1;
+        tmpPosts.length < limitCnt && pageNum <= maxPageNum;
+        pageNum++
+      ) {
+        const { data } = await fetchPostList(pageNum);
+        // console.log(keyword);
+        // console.log(data);
+
+        for (let i = 0; i < data.tistory.item.posts.length; i++) {
+          const postData = data.tistory.item.posts[i];
+          if (
+            postData.visibility == '20' &&
+            postData.title.indexOf(keyword) > -1
+          ) {
+            tmpPosts.push(postData);
+            if (tmpPosts.length == limitCnt) break;
+          }
+        }
+      }
+      console.log(tmpPosts);
     },
   },
   created() {
     // console.log(this.$route.params.categoryId);
+    // console.log($vm.data.postCnt);
     this.fetchPostByCategory();
   },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+div.posts {
+  position: relative;
+  width: 80%;
+  height: calc(100% - 60px);
+}
+ul {
+  margin: 30px 0;
+  list-style: none;
+}
+</style>
