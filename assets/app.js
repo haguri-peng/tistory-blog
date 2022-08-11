2bfd9d1e2e0e6853ff30a9de0afbe79440dc4025
haguri <hty851111@naver.com>
2022-08-11 14:49:52 +0900

    fontawesome 적용

--- assets/app.js
+++ assets/app.js
@@ -15,9 +15,23 @@ import { createApp } from 'vue';
 import App from '../components/App.vue';
 import router from '../router/index';
 
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+/* import specific icons */
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+/* add icons to the library */
+library.add(fas, far, fab);
+
 const app = createApp({
   components: {
     App,
   },
 });
-app.use(router).mount('#app');
+app.use(router).component('font-awesome-icon', FontAwesomeIcon).mount('#app');
