ee3dfbb6837f1725118928c4c6a39910a53a096b
haguri <hty851111@naver.com>
2022-08-11 21:42:54 +0900

    post를 table의 row 형태로 보이게 설정

--- components/AppPost.vue
+++ components/AppPost.vue
@@ -1,12 +1,14 @@
 <template>
-  <li>
-    <div class="post-id">ID: {{ postItem.id }}</div>
-    <div class="post-title">
+  <li @click="clickPost(postItem.id)">
+    <span class="post-id">ID: {{ postItem.id }}</span>
+    <span class="post-title">
       <span>{{ postItem.title }}</span>
-      <span>[{{ postItem.comments }}]</span>
-    </div>
-    <div class="post-url">URL: {{ postItem.postUrl }}</div>
-    <div class="post-date">작성일자: {{ postItem.date }}</div>
+      <span style="margin-left: 5px; font-size: 1rem">
+        [{{ postItem.comments }}]
+      </span>
+    </span>
+    <span class="post-url">{{ postItem.postUrl }}</span>
+    <span class="post-date">{{ postItem.date }}</span>
   </li>
 </template>
 
@@ -18,20 +20,36 @@ export default {
       required: true,
     },
   },
+  methods: {
+    clickPost(id) {
+      console.log('postId >> ' + id);
+    },
+  },
 };
 </script>
 
 <style scoped>
 li {
-  display: inline-block;
-  width: 40%;
-  border: 3px solid #73a9ad;
-  margin: 10px 10px;
-  opacity: 0.8;
+  display: table;
+  width: 100%;
+  /* border: 3px solid #73a9ad; */
+  font-size: 1.5rem;
+  margin: 2px 10px;
+  /* opacity: 0.8; */
 }
 li:hover {
-  border: 3px solid darkcyan;
-  opacity: 1;
+  /* border: 3px solid darkcyan; */
+  background-color: rgb(254, 251, 231, 0.4);
+  color: lightcoral;
+  /* opacity: 1; */
   cursor: pointer;
+}
+span.post-id,
+span.post-url {
+  display: none;
+}
+span.post-date {
+  margin-left: 10px;
+  display: none;
 }
 </style>
