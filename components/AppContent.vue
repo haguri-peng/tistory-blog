9cfc8eb86f05f653e3fb8c0788058efc8953b97c
haguri <hty851111@naver.com>
2022-08-17 18:09:07 +0900

    태그 위치 이동, 댓글 영역 추가

--- components/AppContent.vue
+++ components/AppContent.vue
@@ -3,25 +3,6 @@
     <h1 class="title">{{ title }}</h1>
     <!-- <div v-html="content"></div> -->
     <app-content-main :content="content"></app-content-main>
-    <div class="top-down" v-show="isContent">
-      <div @click="gotoTop">
-        <font-awesome-icon
-          icon="fa-solid fa-arrow-up"
-          size="2x"
-          title="top"
-          bounce
-          style="cursor: pointer"
-        />
-      </div>
-      <!-- <div style="margin-top: 10px">
-        <font-awesome-icon
-          icon="fa-solid fa-rotate-left"
-          size="2x"
-          title="back"
-          style="cursor: pointer"
-        />
-      </div> -->
-    </div>
     <div class="tags">
       <span class="tag" v-for="(tag, index) in tags" :key="index">
         <span class="title" v-if="index === 0">
@@ -34,7 +15,58 @@
         #{{ tag }}
       </span>
     </div>
-    <div class="comments"></div>
+    <div class="comments">
+      <p v-show="comments">
+        {{ comments.length }} Comments
+        <font-awesome-icon icon="fa-solid fa-comments" />
+        <button type="button" style="float: right" @click="addComment">
+          <font-awesome-icon icon="fa-solid fa-pen" title="댓글 등록" />
+          등록
+        </button>
+      </p>
+      <div
+        v-for="comment in comments"
+        :key="comment.id"
+        :class="{ hide: comment.open == 'N' }"
+      >
+        <div class="name">
+          <font-awesome-icon
+            icon="fa-solid fa-house-user"
+            :title="comment.name"
+          />
+          <span>{{ comment.name }}</span>
+          <span style="margin-left: 30px; font-size: small">
+            {{ comment.date }}
+          </span>
+        </div>
+        <div class="comment" v-if="comment.visibility == '0'">
+          승인 대기중인 댓글입니다.
+        </div>
+        <div class="comment" v-else>
+          {{ comment.comment }}
+        </div>
+      </div>
+    </div>
+    <div class="top-down" v-show="isContent">
+      <div @click="gotoTop">
+        <font-awesome-icon
+          icon="fa-solid fa-circle-up"
+          size="lg"
+          title="top"
+          bounce
+          style="cursor: pointer"
+        />
+      </div>
+      <div @click="gotoComments" style="margin-top: 10px">
+        <font-awesome-icon
+          icon="fa-solid fa-comment"
+          size="lg"
+          title="comments"
+          flip
+          style="cursor: pointer; --fa-animation-duration: 3s"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
@@ -79,8 +111,15 @@ export default {
         this.comments = data.tistory.item.comments;
       }
     },
+    addComment() {
+      alert('작업중입니다!!');
+    },
     gotoTop() {
       window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
+    },
+    gotoComments() {
+      const commentsEl = document.querySelector('div.comments');
+      commentsEl.scrollIntoView({ behavior: 'smooth' });
     },
   },
   created() {
@@ -88,8 +127,7 @@ export default {
 
     this.getContent();
     this.getComments();
-  },
-  updated() {
+
     const headerHeight = 60;
     const contentTopMargin = 30;
     const contentInnerPadding = 20;
@@ -132,7 +170,7 @@ div.content {
   margin-top: 30px;
   padding: 10px;
   top: 60px;
-  width: 80%;
+  width: 70%;
   /* background-color: rgba(144, 200, 172, 0.3); */
   animation-name: fadeInUp;
   animation-duration: 1.1s;
@@ -154,5 +192,42 @@ div.tags {
 }
 div.tags span.tag {
   margin-right: 5px;
+}
+div.comments {
+  margin-top: 30px;
+  width: 80%;
+  color: darkslategray;
+}
+div.comments p {
+  text-align: left;
+  border-bottom: 3px solid #76549a;
+  padding-bottom: 4px;
+}
+div.comments > div {
+  padding: 10px;
+  border-bottom: 1px solid #76549a;
+}
+div.comments > div.hide {
+  display: none;
+}
+div.comments > div > div.name {
+  display: flex;
+  align-items: center;
+  margin-bottom: 5px;
+  padding-left: 40px;
+}
+div.comments > div > div.comment {
+  text-align: left;
+  padding-left: 80px;
+}
+button {
+  pointer-events: all;
+  padding: 2px 5px;
+  cursor: pointer;
+  background-color: rgba(118, 84, 154, 0.5);
+  border-radius: 3px;
+}
+button:hover {
+  background-color: rgba(223, 120, 97, 0.5);
 }
 </style>
