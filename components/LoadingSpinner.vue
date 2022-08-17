878c90a9a2f28d671e6c202703e6a452aa1fb072
haguri <hty851111@naver.com>
2022-08-17 18:02:09 +0900

    CSS 보완

--- components/LoadingSpinner.vue
+++ components/LoadingSpinner.vue
@@ -19,8 +19,8 @@ export default {};
   width: 50px;
   height: 50px;
   border-radius: 50%;
-  border: 5px solid #a0bcc2;
-  border-bottom: 5px solid #f9ebc8;
+  border: 5px solid #76549a;
+  border-bottom: 5px solid #df7861;
   animation: spin 1s linear infinite;
   position: relative;
 }
