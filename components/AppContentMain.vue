668e179ca1e1f407ae0b3a578732d917ee7eb910
haguri <hty851111@naver.com>
2022-09-06 13:30:55 +0900

    불필요한 부분 삭제. adFit 관련 정규식 및 태그 만들어주는 부분 보완

--- components/AppContentMain.vue
+++ components/AppContentMain.vue
@@ -15,22 +15,24 @@ function adfitLoader() {
   if (typeof window['adfit'] === 'function') {
     return Promise.resolve();
   } else {
-    return loadScript('https://t1.daumcdn.net/kas/static/ba.min.js', 'async');
+    return loadScript('//t1.daumcdn.net/kas/static/ba.min.js', 'async');
   }
 }
 
-function getAdfitHtml(id) {
+const adfitMiddleId = 'DAN-OrpQV5Dh13ppgSiy';
+function getAdfitHtml() {
   const width = '728px';
   const height = '90px';
 
   let sAdfitHtml = '';
-  sAdfitHtml += '<ins class="kakao_ad_area" ';
-  // sAdfitHtml +=
-  //   'style="display: inline-block; height: 90px; max-height: inherit; min-height: 90px; min-width: 728px; width: 728px; text-decoration: none;" ';
-  sAdfitHtml += 'data-ad-unit="' + id + '" ';
-  sAdfitHtml += 'data-ad-width="' + width + '" ';
-  sAdfitHtml += 'data-ad-height="' + height + '">';
-  sAdfitHtml += '</ins>';
+  sAdfitHtml +=
+    '    <ins class="kakao_ad_area" style="display: inline-block; height: 90px; max-height: inherit; min-height: 90px; min-width: 728px; width: 728px; text-decoration: none;" data-ad-unit="' +
+    adfitMiddleId +
+    '" data-ad-width="' +
+    width +
+    '" data-ad-height="' +
+    height +
+    '"></ins>';
 
   return sAdfitHtml;
 }
@@ -47,52 +49,33 @@ export default {
     content() {
       const dom = htmlparser2.parseDocument(this.content);
 
-      if (dom != null) {
-        // // Twitter Widget이 있으면 로딩
-        // const twtrEl = _.filter(dom.children, ['name', 'blockquote']);
-        // if (twtrEl.length > 0) {
-        //   // Tweet widget 보이게 설정
-        //   twttr.widgets.load();
-        // }
+      if (dom != null && !this.isUpdated) {
+        let modifiedContent = this.content;
 
-        // // 코드 영역이 있으면 구문 강조
-        // const codeEl = _.filter(
-        //   dom.children,
-        //   (c) => c.name == 'div' && c.attributes[0].name == 'v-highlight'
-        // );
-        // if (codeEl.length > 0) {
-        //   setTimeout(() => {
-        //     Prism.highlightAll();
-        //   }, 500);
-        // }
-
-        if (!this.isUpdated) {
-          let modifiedContent = this.content;
-
-          // 광고 adfit
-          const ads = _.filter(
-            dom.children,
-            (c) => c.name == 'figure' && c.attribs.class == 'ad-wp'
-          );
-          console.log(ads);
-          if (ads.length > 0) {
-            for (let i = 0; i < ads.length; i++) {
-              console.log(getAdfitHtml(ads[i].attribs['data-ad-id-pc']));
-              if (ads[i].attribs['data-ad-vendor'] == 'adfit') {
-                modifiedContent = modifiedContent.replace(
-                  '/<figure[^>]*>*.data-ad-id-pc="(' +
-                    ads[i].attribs['data-ad-id-pc'] +
-                    ').*<\/figure>/g',
-                  getAdfitHtml(ads[i].attribs['data-ad-id-pc'])
-                );
-              }
+        // 광고 adfit
+        const ads = _.filter(
+          dom.children,
+          (c) => c.name == 'figure' && c.attribs.class == 'ad-wp'
+        );
+        // console.log(ads);
+        if (ads.length > 0) {
+          for (let i = 0; i < ads.length; i++) {
+            if (ads[i].attribs['data-ad-vendor'] == 'adfit') {
+              const regex = new RegExp(
+                '<figure[^>]*.data-ad-id-pc="(' +
+                  ads[i].attribs['data-ad-id-pc'] +
+                  ').*><\/figure>'
+              );
+              modifiedContent = modifiedContent.replace(
+                regex,
+                getAdfitHtml(ads[i].attribs['data-ad-id-pc'])
+              );
             }
-            //
           }
-
-          this.isUpdated = true;
-          this.modContent = modifiedContent;
         }
+
+        this.isUpdated = true;
+        this.modContent = modifiedContent;
       }
     },
     modContent() {
